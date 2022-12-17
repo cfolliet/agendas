b480a4e45bd43cc63a61017e7e0cda8f3cb48ae9
Clément Folliet <clement.folliet@gmail.com>
2022-12-17 17:03:35 +0000

    a

--- main.js
+++ main.js
@@ -13,13 +13,28 @@ function Agendas() {
     }
 }
 
-function Meeting(day, start, end, _gests) {
+function Meeting(_name, _gests, _day, _start, _duration) {
+    // duration in minutes
+    let name = _name
     let guests = _gests; // ['guest1', 'guests2', ...]
-    let interval = [day, start, end]; // [day, start, end] == [0, 900, 1030] == monday from 9 to 10:30
+    let day = _day
+    let start = _start
+    let end = _start + _duration
+    let duration = _duration
 
-    return { guests, interval }
+    return { name, guests, day, start, end, duration }
 }
 
+function defineNeeds() {
+    let n = [
+        ['1:1 theo', ['clement', 'theo'], 30],
+        ['1:1 maxime', ['clement', 'maxime'], 30],
+        ['squad meeting', ['clement', 'theo','maxime'], 60],
+        ['EM chapter', ['clement', 'virgile'], 45]
+    ]
+
+    return n;
+}
 
 function Tests() {
 
@@ -27,6 +42,14 @@ function Tests() {
         let a = Agendas()
         a.addMeeting()
         if (a.meetings.length !== 1) throw 'addMeeting'
+    }
+
+    function setTime (t) {
+        // todo get a time "9:30" or "14:15" and translate it into nb of minutes starting at "0:00"
+    }
+
+    function getTime (t) {
+        // todo nb of minutes starting at "0:00" translate it into a time like "9:30" or "14:15"
     }
 
     function run() {
