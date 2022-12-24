e5f5282f24d371e16ffd04ece1843da4e93dc212
Clément Folliet <clement.folliet@gmail.com>
2022-12-24 15:16:36 +0000

    Add Fitness + fix tostring + add randomDay

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function Agendas() {
 
     function toString() {
         meetings.forEach(m => {
-            console.log(m.name, utils().getTime(m.start), '-', utils().getTime(m.end), m.guests)
+            m.toString()
         })
     }
 
@@ -40,15 +40,20 @@ function utils() {
         return durations[random];
     }
 
-    function getRandomTime(){
+    function getRandomTime() {
         return Math.floor((Math.random() * 96)) * 15; // 96*15min in a day
+    }
+
+    function getRandomDay() {
+        return Math.floor((Math.random() * 3)) // 3 days workweek
     }
 
     return {
         getTime,
         setTime,
         getRandomDuration,
-        getRandomTime
+        getRandomTime,
+        getRandomDay
     }
 }
 
@@ -61,15 +66,19 @@ function Meeting(_name, _gests, _day, _start, _duration) {
     let end = _start + _duration
     let duration = _duration
 
-    return { name, guests, day, start, end, duration }
+    function toString() {
+        console.log(name, day, utils().getTime(start), '-', utils().getTime(end), guests)
+    }
+
+    return { name, guests, day, start, end, duration, toString }
 }
 
 function defineNeeds() {
     let n = [
-        ['1:1 theo', ['clement', 'theo'], 30],
-        ['1:1 maxime', ['clement', 'maxime'], 30],
-        ['squad meeting', ['clement', 'theo', 'maxime'], 60],
-        ['EM chapter', ['clement', 'virgile'], 45]
+        ['1:1 theo', ['clement', 'theo'], 29],
+        ['1:1 maxime', ['clement', 'maxime'], 29],
+        ['squad meeting', ['clement', 'theo', 'maxime'], 59],
+        ['EM chapter', ['clement', 'virgile'], 44]
     ]
 
     return n;
@@ -80,14 +89,34 @@ function Generator() {
     function generateRandom(needs) {
         let a = Agendas()
         needs.forEach(n => {
-            let m = Meeting(n[0], n[1], 0, utils().getRandomTime(), utils().getRandomDuration(), n[2])
+            let m = Meeting(n[0], n[1], utils().getRandomDay(), utils().getRandomTime(), n[2])
             a.addMeeting(m)
         })
         return a
     }
 
+    function fitnessFunction(agenda) {
+        let score = 0
+
+        agenda.meetings.forEach(m => {
+            agenda.meetings.forEach(m2 => {
+                let isSameMeeting = m == m2
+                if (!isSameMeeting) {
+                    let isSameTime = m.day == m2.day && (m.start <= m2.end && m2.start <= m.end)
+                    let sameGuests = m.guests.filter(g => m2.guests.includes(g)).length;
+                    if (!isSameTime || !sameGuests) {
+                        score++
+                    }
+                }
+            })
+        })
+
+        return score
+    }
+
     return {
-        generateRandom
+        generateRandom,
+        fitnessFunction
     }
 }
 
@@ -95,7 +124,7 @@ function Tests() {
 
     function addMeeting() {
         let a = Agendas()
-        a.addMeeting()
+        a.addMeeting(null)
         if (a.meetings.length !== 1) throw 'addMeeting'
     }
 
@@ -117,10 +146,35 @@ function Tests() {
         if (utils().getTime(1230) != '20:30') throw 'getTime'
     }
 
+    function fitnessFunction() {
+        let a = Agendas()
+        let m = Meeting("a", ['a', 'b'], 0, 00, 59)
+        let m2 = Meeting("b", ['c', 'd'], 0, 60, 59)
+        let m3 = Meeting("c", ['a', 'd'], 0, 120, 59)
+        let m4 = Meeting("d", ['a', 'b'], 1, 00, 59)
+
+        let m5 = Meeting("e", ['a', 'e'], 0, 00, 59)
+        let m6 = Meeting("f", ['c', 'f'], 0, 90, 59)
+        a.addMeeting(m)
+        a.addMeeting(m2)
+        if (2 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 2*2-2
+        a.addMeeting(m3)
+        if (6 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 3*3-3
+        a.addMeeting(m4)
+        if (12 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 4*4-4
+        a.addMeeting(m5)
+        if (18 != Generator().fitnessFunction(a)) throw 'fitnessFunction' //5*5-5-2
+        a.addMeeting(m6)
+        if (26 != Generator().fitnessFunction(a)) throw 'fitnessFunction' //6*6-6-2-2 = nbevent*nbevent-nbevent-2eventinsametime-2othereventinsametime
+        //a = Agendas()
+
+    }
+
     function run() {
         addMeeting(null)
         getTime()
         setTime()
+        fitnessFunction()
         console.log('all good')
     }
 
@@ -132,4 +186,7 @@ function Tests() {
 
 
 Tests().run()
-Generator().generateRandom(defineNeeds()).toString()
+let g = Generator()
+let a = g.generateRandom(defineNeeds())
+a.toString()
+console.log(g.fitnessFunction(a))
