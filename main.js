38896427c696bf9d9d6b569305a46730bcf2af15
Clément Folliet <clement.folliet@gmail.com>
2022-12-29 21:10:28 +0000

    Use GenAlgo and change model

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-
-
-function Agendas() {
-    let meetings = [];
-
-    function addMeeting(m) {
-        meetings.push(m)
-    }
+function Agenda(_times) {
+    const meetings = defineNeeds()
+    const times = _times || [] // pair of {day, timeInMinutes} for each meeting
 
     function toString() {
-        meetings.sort((a, b) => a.start - b.start).sort((a, b) => a.day - b.day).forEach(m => {
-            m.toString()
+        meetings.map((m, i) => {
+            return { meeting: m, time: times[i] }
         })
+            .sort((a, b) => a.time.time - b.time.time)
+            .sort((a, b) => a.time.day - b.time.day)
+            .map(m => {
+                console.log(m.meeting.name, m.time.day, utils().getTime(m.time.time), '-', utils().getTime(m.time.time + m.meeting.duration), m.meeting.guests)
+            })
     }
 
-    return {
-        meetings,
-        addMeeting,
-        toString
-    }
+    return { meetings, times, toString }
 }
 
 function utils() {
@@ -57,22 +53,6 @@ function utils() {
     }
 }
 
-function Meeting(_name, _gests, _day, _start, _duration) {
-    // duration in minutes
-    let name = _name
-    let guests = _gests; // ['guest1', 'guests2', ...]
-    let day = _day
-    let start = _start
-    let end = _start + _duration
-    let duration = _duration
-
-    function toString() {
-        console.log(name, day, utils().getTime(start), '-', utils().getTime(end), guests)
-    }
-
-    return { name, guests, day, start, end, duration, toString }
-}
-
 function defineNeeds() {
     let n = [
         ['1:1 theo', ['clement', 'theo'], 29],
@@ -84,146 +64,145 @@ function defineNeeds() {
     let wttjTeam = ['clement', 'clem', 'robert', 'theo', 'diane', 'victoire', 'pierre', 'maxime', 'arthur', 'aurele']
     let wkteam = ['clement', 'yann', 'sebastien', 'sylvain', 'guillaume', 'mathys', 'isaac', 'jero']
     let n2 = [
-        ['Clem Devs', ['clement', 'clem', 'robert', 'theo', 'diane', 'victoire', 'pierre', 'maxime', 'yann', 'sebastien', 'sylvain', 'guillaume', 'mathys', 'isaac'], 29],
-        ['EM Weekly', ['clement', 'virgile', 'florian'], 44],
-        ['1:1 clem', ['clement', 'clem'], 29],
-        ['Daily WTTJ', wttjTeam, 14], //todo x3
-        ['Daily WK', wkteam, 14], //todo x3
-        ['1:1 seb', ['clement', 'sebastien'], 29],
-        ['1:1 gui', ['clement', 'guillaume'], 29],
-        ['1:1 diane', ['clement', 'diane'], 29],
-        ['1:1 sylvain', ['clement', 'sylvain'], 29],
-        ['Squad Workshop', wttjTeam, 59],
-        ['WTTJ replenishment', wttjTeam, 29],
-        ['EM Chapter', ['clement', 'florian', 'virgile', 'kleroy'], 59],
-        ['1:1 Maxime', ['clement', 'maxime'], 29],
-        ['1:1 theo', ['clement', 'theo'], 29],
-        ['Monthly stagiaire', ['victoire', 'mathys', 'pierre', 'isaac'], 29],
-        ['Tech manager bimonthly', ['kleroy', 'florian', 'virgile', 'clement'], 89],
-        ['Weekly WK', wkteam, 89],
-        ['weekly jero', ['clement', 'jero'], 29],
-        ['weekly aurele', ['clement', 'aurele', 'arthur'], 29],
-        ['1:1 kleroy', ['clement', 'kleroy'], 29],
-        ['Weekly aurelie', ['clement', 'aurelie'], 29],
-        ['Eng leader', ['clement', 'florian', 'virgile', 'kleroy', 'stephane', 'klacointe', 'david', 'robert', 'pix'], 45],
-        ['1:1 yann', ['clement', 'yann'], 29],
-        ['1:1 robert', ['clement', 'robert'], 29]
+        { name: 'Clem Devs', guests: ['clement', 'clem', 'robert', 'theo', 'diane', 'victoire', 'pierre', 'maxime', 'yann', 'sebastien', 'sylvain', 'guillaume', 'mathys', 'isaac'], duration: 29 },
+        { name: 'EM Weekly', guests: ['clement', 'virgile', 'florian'], duration: 44 },
+        { name: '1:1 clem', guests: ['clement', 'clem'], duration: 29 },
+        { name: 'Daily WTTJ', guests: wttjTeam, duration: 14 }, //todo x3
+        { name: 'Daily WK', guests: wkteam, duration: 14 }, //todo x3
+        { name: '1:1 seb', guests: ['clement', 'sebastien'], duration: 29 },
+        { name: '1:1 gui', guests: ['clement', 'guillaume'], duration: 29 },
+        { name: '1:1 diane', guests: ['clement', 'diane'], duration: 29 },
+        { name: '1:1 sylvain', guests: ['clement', 'sylvain'], duration: 29 },
+        { name: 'Squad Workshop', guests: wttjTeam, duration: 59 },
+        { name: 'WTTJ replenishment', guests: wttjTeam, duration: 29 },
+        { name: 'EM Chapter', guests: ['clement', 'florian', 'virgile', 'kleroy'], duration: 59 },
+        { name: '1:1 Maxime', guests: ['clement', 'maxime'], duration: 29 },
+        { name: '1:1 theo', guests: ['clement', 'theo'], duration: 29 },
+        { name: 'Monthly stagiaire', guests: ['victoire', 'mathys', 'pierre', 'isaac'], duration: 29 },
+        { name: 'Tech manager bimonthly', guests: ['kleroy', 'florian', 'virgile', 'clement'], duration: 89 },
+        { name: 'Weekly WK', guests: wkteam, duration: 89 },
+        { name: 'weekly jero', guests: ['clement', 'jero'], duration: 29 },
+        { name: 'weekly aurele', guests: ['clement', 'aurele', 'arthur'], duration: 29 },
+        { name: '1:1 kleroy', guests: ['clement', 'kleroy'], duration: 29 },
+        { name: 'Weekly aurelie', guests: ['clement', 'aurelie'], duration: 29 },
+        { name: 'Eng leader', guests: ['clement', 'florian', 'virgile', 'kleroy', 'stephane', 'klacointe', 'david', 'robert', 'pix'], duration: 44 },
+        { name: '1:1 yann', guests: ['clement', 'yann'], duration: 29 },
+        { name: '1:1 robert', guests: ['clement', 'robert'], duration: 29 }
     ]
 
     return n2;
 }
 
-function Generator() {
-
-    function generateRandom(needs) {
-        let a = Agendas()
-        needs.forEach(n => {
-            let m = Meeting(n[0], n[1], utils().getRandomDay(), utils().getRandomTime(), n[2])
-            a.addMeeting(m)
+function getRandomAgendas(nb) {
+    const agendas = []
+    for (let index = 0; index < nb; index++) {
+        const agenda = Agenda()
+        agenda.meetings.forEach(() => {
+            agenda.times.push({ day: utils().getRandomDay(), time: utils().getRandomTime() })
         })
-        return a
+        agendas.push(agenda)
     }
+    return agendas
+}
 
-    function fitnessFunction(agenda) {
-        let score = 0
+function fitness(agenda) {
+    let score = 0
 
-        agenda.meetings.forEach(m => {
-            let startDay = 9 * 60 // 9h
-            let endDay = 18 * 60 // 18h
-            let isWorkingInWorkingHours = m.end <= endDay && startDay <= m.start
-            if (isWorkingInWorkingHours) {
-                agenda.meetings.forEach(m2 => {
-                    let isSameMeeting = m == m2
-                    if (!isSameMeeting) {
-                        let isSameTime = m.day == m2.day && (m.start <= m2.end && m2.start <= m.end)
-                        let sameGuests = m.guests.filter(g => m2.guests.includes(g)).length;
-                        if (!isSameTime || !sameGuests) {
-                            score++
-                        }
+    agenda.meetings.forEach((m, i) => {
+        let startDay = 9 * 60 // 9h
+        let endDay = 18 * 60 // 18h
+        const day = agenda.times[i].day
+        const start = agenda.times[i].time
+        const end = agenda.times[i].time + m.duration
+        let isWorkingInWorkingHours = end <= endDay && startDay <= start
+        if (isWorkingInWorkingHours) {
+            agenda.meetings.forEach((m2, i2) => {
+                let isSameMeeting = m == m2
+                if (!isSameMeeting) {
+
+                    const day2 = agenda.times[i2].day
+                    const start2 = agenda.times[i2].time
+                    const end2 = agenda.times[i2].time + m.duration
+
+                    let isSameTime = day == day2 && (start <= end2 && start2 <= end)
+                    let sameGuests = m.guests.filter(g => m2.guests.includes(g)).length;
+                    if (!isSameTime || !sameGuests) {
+                        score++
                     }
-                })
-            }
+                }
+            })
+        }
 
-        })
+    })
 
-        return score
-    }
-
-    return {
-        generateRandom,
-        fitnessFunction
-    }
+    return score
 }
 
-function Tests() {
+import pkg from 'genalgo';
+const { GenAlgo, lesser, tournament3Single, tournament3Pair } = pkg;
 
-    function addMeeting() {
-        let a = Agendas()
-        a.addMeeting(null)
-        if (a.meetings.length !== 1) throw 'addMeeting'
+// Create a GenAlgo object with simple parameters
+const algo = new GenAlgo({
+    mutationProbability: 0.8,
+    crossoverProbability: 0.2,
+    iterationNumber: 25,
+    resultSize: 1
+});
+
+// Function used to mutate an individual
+const mutation = agenda => {
+    let a = Agenda(agenda.times)
+
+    a.times.forEach(t => {
+        if (Math.random() > 0.1) {
+            t.time += 30
+        }
+    })
+
+    return a
+};
+
+// Function used to crossover two individuals
+const crossover = (agenda1, agenda2) => {
+    let child1 = Agenda(agenda1.times)
+    let child2 = Agenda(agenda2.times)
+
+    for (let index = 0; index < agenda1.meetings.length; index++) {
+        if (index % 0) {
+            child1.times[index] = child2.times[index]
+        } else {
+            child2.times[index] = child1.times[index]
+        }
     }
 
-    function setTime() {
-        // get a time "9:30" or "14:15" and translate it into nb of minutes starting at "0:00"
-        if (utils().setTime('00:30') != 30) throw 'setTime'
-        if (utils().setTime('01:00') != 60) throw 'setTime'
-        if (utils().setTime('02:00') != 120) throw 'setTime'
-        if (utils().setTime('20:00') != 1200) throw 'setTime'
-        if (utils().setTime('20:30') != 1230) throw 'setTime'
-    }
+    return [child1, child2];
+};
 
-    function getTime() {
-        // nb of minutes starting at "0:00" translate it into a time like "9:30" or "14:15"
-        if (utils().getTime(30) != '00:30') throw 'getTime'
-        if (utils().getTime(60) != '01:00') throw 'getTime'
-        if (utils().getTime(120) != '02:00') throw 'getTime'
-        if (utils().getTime(1200) != '20:00') throw 'getTime'
-        if (utils().getTime(1230) != '20:30') throw 'getTime'
-    }
+// Will be called at each iteration
+const iterationCallback = ({
+    bestIndividual,
+    elapsedTime,
+    iterationNumber
+}) => {
+    console.log("Iteration " + iterationNumber);
+    console.log("Best fitness : " + bestIndividual.fitness);
+    console.log("Elapsed time : " + elapsedTime);
+    return true;
+};
 
-    function fitnessFunction() {
-        let a = Agendas()
-        let m = Meeting("a", ['a', 'b'], 0, 540, 59)
-        let m2 = Meeting("b", ['c', 'd'], 0, 600, 59)
-        let m3 = Meeting("c", ['a', 'd'], 0, 660, 59)
-        let m4 = Meeting("d", ['a', 'b'], 1, 540, 59)
+algo.setSeed(getRandomAgendas(100));
 
-        let m5 = Meeting("e", ['a', 'e'], 0, 540, 59)
-        let m6 = Meeting("f", ['c', 'f'], 0, 630, 59)
-        a.addMeeting(m)
-        a.addMeeting(m2)
-        if (2 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 2*2-2
-        a.addMeeting(m3)
-        if (6 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 3*3-3
-        a.addMeeting(m4)
-        if (12 != Generator().fitnessFunction(a)) throw 'fitnessFunction' // 4*4-4
-        a.addMeeting(m5)
-        if (18 != Generator().fitnessFunction(a)) throw 'fitnessFunction' //5*5-5-2
-        a.addMeeting(m6)
-        if (26 != Generator().fitnessFunction(a)) throw 'fitnessFunction' //6*6-6-2-2 = nbevent*nbevent-nbevent-2eventinsametime-2othereventinsametime
-        
-        // todo need to add test with meeting out of hours
+algo.setFitnessEvaluator(fitness);
 
-    }
+algo.setMutationFunction(mutation);
 
-    function run() {
-        addMeeting(null)
-        getTime()
-        setTime()
-        fitnessFunction()
-        console.log('all good')
-    }
+algo.setCrossoverFunction(crossover);
 
-    return {
-        run
-    }
+algo.setSelectSingleFunction(tournament3Single);
 
-}
+algo.setSelectPairFunction(tournament3Pair);
 
+algo.setIterationCallback(iterationCallback);
 
-Tests().run()
-let g = Generator()
-let a = g.generateRandom(defineNeeds())
-a.toString()
-//console.log('best score with 24m', 24 * 24 - 24)
-//console.log(g.fitnessFunction(a))
+let result = await algo.start();
+result[0].entity.toString()
